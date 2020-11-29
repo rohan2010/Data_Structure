bbb0a0f6e9ca8cf79c4560a5ff035bfa53b6d029
rohanpant <rohanpant1999@gmail.com>
2020-11-29 23:50:23 +0530

    Add

--- dsInJava/src/Mathematics/countDigits.java
+++ dsInJava/src/Mathematics/countDigits.java
@@ -1,4 +1,18 @@
 package Mathematics;
 
+import java.util.Scanner;
+
 public class countDigits {
+    static int hello(int x){
+       int res = 0;
+       while (x>0){
+           x = x/10;
+           res++;
+       }
+       return res;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(hello(s.nextInt()));
+    }
 }
