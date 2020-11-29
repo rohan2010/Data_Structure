84815b7741a26e98057a600fb7a33552b93f6aef
rohanpant <rohanpant1999@gmail.com>
2020-11-30 00:17:18 +0530

    Add

--- dsInJava/src/Mathematics/palindrome.java
+++ dsInJava/src/Mathematics/palindrome.java
@@ -1,7 +1,25 @@
 package Mathematics;
 
-public class palindrome {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class palindrome {
+   static int pal(int x){
+        int res = 0;
+        while(x!=0){
+            int rem = x%10;
+            res = res*10 + rem;
+            x/=10;
+        }
+        return res;
+
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if(n == pal(n)){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
     }
 }
